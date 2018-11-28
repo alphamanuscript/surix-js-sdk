21949f25dcd156a99a85756956690f3a27fed908
Clément Habinshuti <hello@habbes.xyz>
2018-11-29 01:35:12 +0300

    make client private

--- packages/client/src/project/entities.ts
+++ packages/client/src/project/entities.ts
@@ -1,17 +1,17 @@
-import { ApiEntity, EntityWrapper } from '@surix/data-helpers';
+import { ApiEntity, WrappedEntity, wrapEntity, wrapEntityArray } from '@surix/data-helpers';
 import { AxiosInstance } from 'axios';
 import { ProjectEntities, Query, TagList } from '../types';
 
 export function getProjectEntities (projectId: string, apiClient: AxiosInstance): ProjectEntities {
   return {
-    async get (entityId: string): Promise<EntityWrapper> {
+    async get (entityId: string): Promise<WrappedEntity> {
       const res = await apiClient.get<ApiEntity>(`/projects/${projectId}/entities/${entityId}`);
-      return new EntityWrapper(res.data);
+      return wrapEntity(res);
     },
-    async query(query: Query): Promise<EntityWrapper[]> {
+    async query(query: Query): Promise<WrappedEntity[]> {
       const res = await apiClient.post<ApiEntity[]>(`/projects/${projectId}/entities/query`, query);
       const entities = res.data;
-      return entities.map(entity => new EntityWrapper(entity));
+      return wrapEntityArray(entities);
     }
   };
 }
