9928ffee2fe1fc4ac359ff3b3e2c5ace517334fb
grafitto <kevin@manuscript.live>
2019-06-23 12:45:00 +0300

    Pointed to MVP endpoint

--- packages/client/src/client/client.ts
+++ packages/client/src/client/client.ts
@@ -1,14 +1,11 @@
+import { AxiosInstance } from 'axios';
 import { getApiClient } from '../api';
 import { getProjectApi } from '../project';
 import { Project } from '../types';
-import { AxiosInstance } from 'axios';
 
-export const PRODUCTION_URL = 'https://api.surix.co/api';
-export const STAGING_URL = 'https://surix-staging.herokuapp.com/api';
+export const PRODUCTION_URL = 'https://surix-review3.herokuapp.com/api';
 
 export interface ClientOptions {
-  environment?: 'production' | 'staging';
-  baseUrl?: string;
   keyId: string;
   keySecret: string;
 }
@@ -17,14 +14,9 @@ export class Client {
   private _apiClient: AxiosInstance;
 
   constructor (opts: ClientOptions) {
-    const _opts: ClientOptions = Object.assign({
-      environment: 'production',
-      baseUrl: ''
-    }, opts);
-    let baseUrl = _opts.environment === 'production' ? PRODUCTION_URL : STAGING_URL;
-    baseUrl = _opts.baseUrl || baseUrl;
+    const _opts = { ...opts };
     const apiKey = `${_opts.keyId}:${_opts.keySecret}`;
-    this._apiClient = getApiClient(baseUrl, apiKey);
+    this._apiClient = getApiClient(PRODUCTION_URL, apiKey);
   }
 
   project (id: string): Project {
